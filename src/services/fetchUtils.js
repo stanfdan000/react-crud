a8dbd88d00a91ded509739a34db1072a965e25cc
sirdan <stanford303@gmail.com>
2022-06-24 12:32:32 -0700

    added my files and sign in sign up and logout in fetch

---
+++ src/services/fetchUtils.js
@@ -0,0 +1,22 @@
+import { client, } from './client';
+
+export function getUser() {
+  return client.auth.session();
+}
+export async function signIn(email, password) {
+  const response = await client.auth.signIn({ email, password });
+
+  return response.user;
+}
+
+export async function signUp(email, password) {
+  const response = await client.auth.signUp({ email, password });
+
+  return response.user;
+}
+
+export async function logout() {
+  await client.auth.signOut();
+
+  return window.location.href = '../';
+}
